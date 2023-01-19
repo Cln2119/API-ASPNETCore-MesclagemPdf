549821173b657a892a9b20da741c3e33aa96b4aa
Cleiton Lopes <41643385+Cln2119@users.noreply.github.com>
2023-01-19 16:16:54 -0300

    Projeto Inicial

---
+++ ApiMesclarPdf/Controllers/ConversaoController.cs
@@ -0,0 +1,33 @@
+﻿using ApiMesclarPdf.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ApiMesclarPdf.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ConversaoController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult EnviaArquivo([FromForm] ICollection<IFormFile> arquivo)
+        {        
+            try
+            {                
+                ConverterPDF arquivoPdf = new ConverterPDF();    
+
+                var retFormato = arquivoPdf.ConverterEmByte(arquivo);
+
+                return  File(retFormato, arquivo.FirstOrDefault().ContentType, "Arquivos");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Erro ao realizar a mesclagem. Mais detalhes: " + ex);
+            }            
+        }
+    }
+}
---
+++ ApiMesclarPdf/Model/ConverterPDF.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using RestSharp;
+using System.IO;
+using Renci.SshNet;
+using Microsoft.AspNetCore.Http;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+
+namespace ApiMesclarPdf.Model
+{
+    public class ConverterPDF
+    {
+        public byte[] ConverterEmByte(ICollection<IFormFile> filePaths)
+        {
+            try
+            {
+                List<byte[]> listByte = new List<byte[]>();
+                byte[] result = null;
+
+                foreach (var formFile in filePaths)
+                {
+                    if (formFile.Length > 0)
+                    {
+                        using (var stream = new MemoryStream())
+                        {
+                            formFile.CopyToAsync(stream);
+                            listByte.Add(stream.ToArray());
+                        }
+                    }
+                }
+
+                result = MergePdf(listByte);
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Erro ao converter os arquivos de upload em byte. Mais detalhes " + ex);
+            }
+           
+        }
+
+        public static byte[] MergePdf(List<byte[]> pdfs)
+        {
+            try
+            {
+                byte[] result = null;
+
+                List<PdfSharp.Pdf.PdfDocument> lstDocuments = new List<PdfSharp.Pdf.PdfDocument>();
+                foreach (var pdf in pdfs)
+                {
+                    lstDocuments.Add(PdfReader.Open(new MemoryStream(pdf), PdfDocumentOpenMode.Import));
+                }
+
+                using (PdfDocument outPdf = new PdfDocument())
+                {
+                    for (int i = 1; i <= lstDocuments.Count; i++)
+                    {
+                        foreach (PdfSharp.Pdf.PdfPage page in lstDocuments[i - 1].Pages)
+                        {
+                            outPdf.AddPage(page);
+                        }
+                    }
+
+                    using (MemoryStream mem = new MemoryStream())
+                    {
+                        System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+                        outPdf.Save(mem, false);
+                        result = mem.ToArray();
+                    }
+
+                    return result;
+                }
+            }
+            catch(Exception ex)
+            {
+                throw new Exception("Erro realizar o Merge dos Pdf's. Mais detalhes " + ex);
+            }            
+        }
+    }
+}
